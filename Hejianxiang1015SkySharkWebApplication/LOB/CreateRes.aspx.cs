3218059d91757edf0f606d55cb6c95437cedeb31
Jinmuyan1 <126266267+Jinmuyan1@users.noreply.github.com>
2023-05-14 22:31:29 +0800

    Update CreateRes.aspx.cs

--- Hejianxiang1015SkySharkWebApplication/LOB/CreateRes.aspx.cs
+++ Hejianxiang1015SkySharkWebApplication/LOB/CreateRes.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace FangLiangProject4SkySharkWebApp.LOB
+namespace HejianxiangProject4SkySharkWebApp.LOB
 {
     public partial class CreateRes : System.Web.UI.Page
     {
