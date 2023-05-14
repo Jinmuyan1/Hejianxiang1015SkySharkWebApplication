cfa5cb0b237dd1cfac409c798257c1fd7f2bc5e3
Jinmuyan1 <126266267+Jinmuyan1@users.noreply.github.com>
2023-05-14 22:37:08 +0800

    Update ManageUsers.aspx.cs

--- Hejianxiang1015SkySharkWebApplication/NA/ManageUsers.aspx.cs
+++ Hejianxiang1015SkySharkWebApplication/NA/ManageUsers.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace FangLiangProject4SkySharkWebApp.NA
+namespace HejianxiangProject4SkySharkWebApp.NA
 {
     public partial class ManageUsers : System.Web.UI.Page
     {
